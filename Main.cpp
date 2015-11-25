6756304f09cc2c53cefea5a6d83795d920ac69cf
Michael Hollister <michael.s.hollister@gmail.com>
2015-11-24 21:40:21 -0600

    Adding license boilerplate

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,7 @@
+// This source code is subject to the terms of The MIT License.
+// If a copy of The MIT License was not distributed with this file,
+// you can obtain one at http://opensource.org/licenses/MIT.
+
 #include <iostream>
 #include <string>
 #include <wiringPi.h>
